dd461eacb34d6db18853f1f8ae77b1752b011458
jesseryy <jesse.ryynala@gmail.com>
2022-05-01 23:58:03 +0300

    Tietovisan parantelua

--- scripts/jesse.js
+++ scripts/jesse.js
@@ -1,139 +1,98 @@
-
-const startButton = document.getElementById("start-btn")
-const nextButton = document.getElementById("next-btn")
-
-const questionContainerElement = document.getElementById("question-container")
-const questionElement = document.getElementById("question")
-const answerButtonsElement = document.getElementById("answer-buttons")
-
-let shuffledQuestions, correctQuestionIndex;
-let quizScore = 0;
-
-
-startButton.addEventListener("click", startGame)
-
-nextButton.addEventListener("click", () => {
-    currectQuestionIndex++
-    setnextQuestion()
-})
-
-function startGame() {
-    startButton.classList.add("hide")
-    shuffledQuestions = questions.sort(() => Math.random()  -0.5)
-    currectQuestionIndex = 0;
-    questionContainerElement.classList.remove("hide")
-    setnextQuestion()
-    quizScore = 0
-}
-
-
-
-function setnextQuestion() {
-    resetState();
-    showQuestion(shuffledQuestions[currectQuestionIndex])
-}
-
-
-function showQuestion(question) {
-    questionElement.innerText = question.question;
-    question.answers.forEach((answer) => {
-        const button = document.createElement("button")
-        button.innerText = answer.text;
-        button.classList.add("btn")
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-}
-
-
-
-function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add("hide")
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
-}
-
-
-function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach((button) => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currectQuestionIndex + 1) {
-        nextButton.classList.remove("hide")
-    } else {
-        startButton.innerText = "restart"
-        startButton.classList.remove("hide")
-    }
-    if (selectedButton.dataset = correct) {
-        quizScore++
-    }
-    document.getElementById("right-answers").innerText = quizScore
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add("correct")
-    }
-    else {
-        element.classList.add("wrong")
-    }
-}
-
-
-
-function clearStatusClass(element) {
-    element.classList.remove("correct")
-    element.classList.remove("wrong")
-}
-const questions = [
+const quizData = [
     {
         question: "Antiikin ajaksi kutsutaan...",
-        answers: [
-            { text: "Muinaisen Egyptin valtakunnan aikaa.", correct: false },
-            { text: "Muinaisen Kreikan ja Rooman valtakunnan aikaa.", correct: true },
-            { text: "Ötzin löytymisen aikaa.", correct: false },
-        ],
+        a: "Muinaisen Egyptin valtakunnan aikaa.",
+        b: "Muinaisen Kreikan ja Rooman valtakunnan aikaa.",
+        c: "Ötzin löytymisen aikaa.",
+        correct: "b",
     },
     {
         question: "Antiikin ajanjakso kesti...",
-        answers: [
-            { text: "13-vuotta.", correct: false },
-            { text: "130-vuotta.", correct: false },
-            { text: "1300-vuotta.", correct: true },
-        ],
+        a: "13 vuotta.",
+        b: "130 vuotta.",
+        c: "1300 vuotta.",
+        correct: "c",
     },
     {
         question: "Antiikin aika päättyi, kun...",
-        answers: [
-            { text: "Länsi-Rooma tuhoutui.", correct: true },
-            { text: "Kirjoitustaito keksittiin.", correct: false },
-            { text: "Hieroglyfien arvoitus ratkaistiin.", correct: false },
-        ],
+        a: "Länsi-Rooma tuhoutui.",
+        b: "Kirjoitustaito keksittiin.",
+        c: "Hieroglyfien arvoitus ratkaistiin.",
+        correct: "a",
     },
     {
         question: "Demokratia tarkoittaa sitä, että kansalaiset...",
-        answers: [
-            { text: "Päättävät yhteisistä asioista.", correct: true },
-            { text: "Valitsevat kuninkaan.", correct: false },
-            { text: "Viljelevät maata.", correct: false },
-        ],
+        a: "Päättävät yhteisistä asioista.",
+        b: "Valitsevat kuninkaan.",
+        c: "Viljelevät maata.",
+        correct: "a",
     },
     {
         question: "Antiikin aika näkyy meidän aikanamme...",
-        answers: [
-            { text: "Ruokakulttuurissa.", correct: false },
-            { text: "Terveydenhuollossa.", correct: false },
-            { text: "Rakennustyylissä.", correct: true },
-        ],
+        a: "Ruokakulttuurissa.",
+        b: "Terveydenhuollossa.",
+        c: "Rakennustyylissä.",
+        correct: "c",
     },
 ]
+
+const quiz = document.getElementById("quiz")
+const answerEls = document.querySelectorAll(".answer")
+const questionEl = document.getElementById("question")
+const a_text = document.getElementById("a_text")
+const b_text = document.getElementById("b_text")
+const c_text = document.getElementById("c_text")
+const submitBtn = document.getElementById("submit")
+
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+    
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked){
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+submitBtn.addEventListener("click", () => {
+    const answer = getSelected()
+    if(answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++
+        }
+
+        currentQuiz++
+
+        if(currentQuiz < quizData.length){
+            loadQuiz()
+        } else {
+            quiz.innerHTML = `
+            <h2>Sinä vastasit ${score}/${quizData.length} kysymykseen oikein</h2>
+
+            <button onclick="location.reload()">Kokeile uudestaan</button>
+            `
+        }
+    }
+})
